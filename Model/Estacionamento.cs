4bb1cb9e1fa0c21f856b3fb10fa304f14fcea6ab
vitorliras <vitorlirasti@hotmail.com>
2023-12-12 21:11:35 -0300

    Finalizando a classe estacionamento

--- Model/Estacionamento.cs
+++ Model/Estacionamento.cs
@@ -9,21 +9,53 @@ namespace Model
 
         public void AdicionarVeiculo()
         {
-           
+            Console.Write("Digite a placa do veículo: ");
+            string placa = Console.ReadLine();
+
+            Veiculo novoVeiculo = new Veiculo
+            {
+                Placa = placa,
+                Entrada = DateTime.Now
+            };
+
+            veiculosEstacionados.Add(novoVeiculo);
+
+            Console.WriteLine($"Veículo da placa {placa} foi adicionado com sucesso.");
         }
 
         public void RemoverVeiculo()
         {
-           
+            Console.Write("Informe a placa do veículo que vai ser removido: ");
+            string placa = Console.ReadLine();
+
+            Veiculo veiculo = veiculosEstacionados.Find(v => v.Placa == placa);
+
+            if (veiculo != null)
+            {
+                TimeSpan tempoEstacionado = DateTime.Now - veiculo.Entrada;
+                double valorCobrado = CalcularValorEstacionamento(tempoEstacionado.TotalHours);
+
+                Console.WriteLine($"Veículo {placa} removido. Valor cobrado: R${valorCobrado:F2}");
+                veiculosEstacionados.Remove(veiculo);
+            }
+            else
+            {
+                Console.WriteLine($"Veículo com placa {placa} não encontrado.");
+            }
         }
 
         public void ListarVeiculos()
         {
-            
+            Console.WriteLine("Veículos estacionados:");
+            foreach (var veiculo in veiculosEstacionados)
+            {
+                Console.WriteLine($"Placa: {veiculo.Placa} - Entrada: {veiculo.Entrada}");
+            }
         }
 
         private double CalcularValorEstacionamento(double horasEstacionado)
         {
+            return horasEstacionado * 5.0;
         }
     }
 }
