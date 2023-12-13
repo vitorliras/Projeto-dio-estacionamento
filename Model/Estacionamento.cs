f6623e265e292c74328a1069e4ff49bea994e922
vitorliras <vitorlirasti@hotmail.com>
2023-12-12 21:06:08 -0300

    Comecando com a classe estacionamento

---
+++ Model/Estacionamento.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class Estacionamento
+    {
+        private List<Veiculo> veiculosEstacionados = new List<Veiculo>();
+
+        public void AdicionarVeiculo()
+        {
+           
+        }
+
+        public void RemoverVeiculo()
+        {
+           
+        }
+
+        public void ListarVeiculos()
+        {
+            
+        }
+
+        private double CalcularValorEstacionamento(double horasEstacionado)
+        {
+        }
+    }
+}
